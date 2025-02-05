97b57908849156c4b5a90cf9b5dcb7e43b0451a7
yonggyo1125 <yonggyo00@kakao.com>
2025-02-05 16:22:38 +0900

    .

--- src/app/global/ui/outlines/Header.tsx
+++ src/app/global/ui/outlines/Header.tsx
@@ -1,11 +1,35 @@
 'use client'
 import React from 'react'
+import Link from 'next/link'
 import { styled } from 'styled-components'
+import { SlLogin } from 'react-icons/sl'
+import { FaUserPlus, FaHome, FaSearch } from 'react-icons/fa'
 
 const StyledHeader = styled.header``
 
 const Header = () => {
-  return <StyledHeader>상단영역</StyledHeader>
+  return (
+    <StyledHeader>
+      <div className="site-top">
+        <div className="left">
+          <Link href="/">
+            <FaHome />
+          </Link>
+        </div>
+        <div className="right">
+          <Link href="/member/join">
+            <FaUserPlus /> 회원가입
+          </Link>
+          <Link href="/member/login">
+            <SlLogin /> 로그인
+          </Link>
+        </div>
+      </div>
+      {/* site-top */}
+      <div className="logo-search"></div>
+      <nav></nav>
+    </StyledHeader>
+  )
 }
 
 export default React.memo(Header)
