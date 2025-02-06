ae21fcbdf04dae9b0fe550232b4a11b9c75fa8f8
yonggyo1125 <yonggyo00@kakao.com>
2025-02-06 16:11:36 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from './global/ui/outlines/Header'
 import Footer from './global/ui/outlines/Footer'
 import { CommonProvider } from './global/contexts/CommonContext'
 import { Metadata } from 'next'
+import 'react-datepicker/dist/react-datepicker.css'
 import './globals.css'
 
 export const metadata: Metadata = {
--- src/app/member/join/components/JoinForm.tsx
+++ src/app/member/join/components/JoinForm.tsx
@@ -1,15 +1,116 @@
 import React from 'react'
 import styled from 'styled-components'
+import {
+  MdRadioButtonUnchecked,
+  MdRadioButtonChecked,
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+} from 'react-icons/md'
 
 import { Input } from '@/app/global/components/FormComponents'
+import DatePicker from 'react-datepicker'
 
 const StyledForm = styled.form``
 
-const JoinForm = () => {
+const JoinForm = ({ form, onClick, onChange, onSelectDate }) => {
   return (
     <>
       <StyledForm autoComplete="off">
-        <Input type="text" name="email" placeholder="이메일" />
+        <input type="hidden" name="gender" value={form?.gender ?? ''} />
+
+        <Input
+          type="text"
+          name="email"
+          placeholder="이메일"
+          color="dark"
+          value={form?.email ?? ''}
+          onChange={onChange}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          placeholder="비밀번호"
+          color="dark"
+          value={form?.password ?? ''}
+          onChange={onChange}
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="비밀번호 확인"
+          color="dark"
+          value={form?.confirmPassword ?? ''}
+          onChange={onChange}
+        />
+        <Input
+          type="text"
+          name="name"
+          placeholder="이름"
+          color="dark"
+          value={form?.name ?? ''}
+          onChange={onChange}
+        />
+
+        <div className="address-row">
+          <Input
+            type="text"
+            name="zipCode"
+            placeholder="우편번호"
+            color="dark"
+            value={form?.zipCode ?? ''}
+            onChange={onChange}
+          />
+          <button type="button">주소찾기</button>
+        </div>
+
+        <Input
+          type="text"
+          name="address"
+          placeholder="집주소"
+          color="dark"
+          value={form?.address ?? ''}
+          onChange={onChange}
+        />
+        <Input
+          type="text"
+          name="addressSub"
+          placeholder="나머지 주소"
+          color="dark"
+          value={form?.addressSub ?? ''}
+          onChange={onChange}
+        />
+
+        <div className="row">
+          <div className="tit">성별</div>
+          <div className="radio-buttons">
+            <span onClick={() => onClick('gender', 'FEMALE')}>
+              {form?.gender === 'FEMALE' ? (
+                <MdRadioButtonChecked />
+              ) : (
+                <MdRadioButtonUnchecked />
+              )}
+              여성
+            </span>
+            <span onClick={() => onClick('gender', 'MALE')}>
+              {form?.gender === 'MALE' ? (
+                <MdRadioButtonChecked />
+              ) : (
+                <MdRadioButtonUnchecked />
+              )}
+              남성
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="tit">생년월일</div>
+          <div>
+            <DatePicker
+              selected={form?.birthDt ?? ''}
+              onChange={(date) => onSelectDate(date)}
+            />
+          </div>
+        </div>
       </StyledForm>
     </>
   )
