cc12f3d0431c8aa1cf142d840eb7641fc2027543
yonggyo1125 <yonggyo00@kakao.com>
2025-02-06 16:53:01 +0900

    .

---
+++ src/app/member/services/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+/**
+ * 회원가입 처리
+ * @param params : 쿼리스트링값
+ * @param formData
+ */
+export const processJoin = async (params, formData: FormData) => {}
+
+/**
+ * 로그인 처리
+ *
+ * @param params
+ * @param formData
+ */
+export const processLogin = async (params, formData: FormData) => {}
